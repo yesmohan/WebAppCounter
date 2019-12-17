09437d46bd5b22c16bca597e893c2b82644d0fed
sekarvenkatachalam <57977192+sekarvenkatachalam@users.noreply.github.com>
2019-12-17 11:56:59 +0530

    Update monitorting.sh

--- monitorting.sh
+++ monitorting.sh
@@ -8,7 +8,7 @@ farm_url=http://192.168.0.108:8080/my-farm/index.jsp
 
 # save the status in some variable 
 
-status=`curl $farm_url -k -s -f -o /dev/null && echo "FARM SERVER IS UP" || echo "SOMETHING WENT BAD - ERROR"`    
+status=`curl $farm_url -k -s -f -o /dev/null && echo "FARM SERVER IS UP , check $farm_url" || echo "SOMETHING WENT BAD - ERROR"`    
 
 # print results
 
